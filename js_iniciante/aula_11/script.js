f97c7c6b652eada6a3f79ac64fc0a60191470337
juliastella <juliastellasil1@gmai.com>
2023-10-02 18:39:10 -0300

    primeiro commit

---
+++ js_iniciante/aula_11/script.js
@@ -0,0 +1,10 @@
+let num1 = prompt('digite um numero');
+let num2 = prompt('digite um numero');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+
+
+const resultado = (num1 + num2);
+alert(`O resultado é: ${resultado}`);
---
+++ js_iniciante/aula_9/index.js
@@ -0,0 +1,24 @@
+/**
+ * Diferença entre let e var
+
+Com let você consegue redeclara a variavel já com var não.
+
+*/
+
+var nome = 'Júlia';
+
+//let nome = 2;
+
+/**
+ * Incremento
+
+Não podemos colocar em uma const uma contrudo de incremento, pois é algo que irá mudar.
+
+*/
+
+let contador = 1;
+
+contador++;
+
+console.log(contador);
+
