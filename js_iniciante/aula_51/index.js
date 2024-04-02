563e680459b23fde5a3848cb55eb44fb75e235bb
Julia Stella <julia.paula@liferay.com>
2024-04-02 14:31:16 -0300

    Finalização da destruturaçào

--- js_iniciante/aula_51/index.js
+++ js_iniciante/aula_51/index.js
@@ -11,4 +11,15 @@ const pessoa = {
 const {idade} = pessoa // vai tirar co objeto essa chave
 //const {nome = 'Não existe', sobrenome} = pessoa // podemos colocar mais de uma atributo do metodo. Além de atribuir valores a eles.
 const {nome: teste = 'Não existe', sobrenome} = pessoa // o nome terá outro valor
+
+//const { endereco: {rua, numero}, endereco } = pessoa; // E eu posso pergar para mostrar o objeto novamente, assim declarei endereço de novo
 console.log(teste, sobrenome);
+
+const { endereco: {rua:r = 12345 , numero}, endereco } = pessoa;
+
+console.log(r, numero, endereco);
+
+// podemos também pegar o resto do objeto
+
+const {nome, ...resto } = pessoa;
+console.log(resto);
