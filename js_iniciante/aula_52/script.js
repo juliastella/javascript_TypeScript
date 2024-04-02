9b373212d5ec8d55bfbd203c3a3baee0fb6c7655
Julia Stella <julia.paula@liferay.com>
2024-04-02 14:56:46 -0300

    For classico

---
+++ js_iniciante/aula_52/script.js
@@ -0,0 +1,19 @@
+/**
+ * FOR
+ */
+
+// Exemplo para ver se é impar ou par
+
+for (let i = 0; i <= 10; i++){
+    const par = i % 2 === 0 ? 'par' : 'impar';
+    console.log(i, par);
+}
+
+// percorrer um array
+const frutas = ['maçã', 'pera', 'uva']
+
+//length retorna o tamanho do array
+
+for (let i = 0; i < frutas.length; i++){
+    console.log(`Índice ${i}`, frutas[i]);
+}
