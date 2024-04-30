58410c6cb23292015f55c02dbf90279b6ca2337c
Julia Stella <julia.paula@liferay.com>
2024-04-30 12:50:51 -0300

    execicio 66

--- js_iniciante/execicios/execicio_66/assets/js/main.js
+++ js_iniciante/execicios/execicio_66/assets/js/main.js
@@ -1,16 +1,46 @@
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
+function relogio() {
+  function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000); 
+    return data.toLocaleTimeString('pt-BR', {
+      hour12: false,
+      timeZone: 'UTC'
+    });
+  }
+  
+  
+  const relogio = document.querySelector('.relogio');
+  
+  let segundos = 0; 
+  let time;
+  
+  function iniciaRelogio() {
+    time = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000); 
+  }
+  
+  document.addEventListener('click', function (e) {
+    const el = e.target;
+  
+    if (el.classList.contains('zerar')) {
+      clearInterval(time);
+      relogio.innerHTML = '00:00:00';
+      relogio.classList.remove('pausado');
+      segundos = 0;
+    }
+  
+    if (el.classList.contains('pausar')) {
+      relogio.classList.add('pausado');
+      clearInterval(time);
+    }
+  
+    if (el.classList.contains('iniciar')) {
+      clearInterval(time);
+      relogio.classList.remove('pausado');
+      iniciaRelogio();
+    }
+  });
+}
 
-iniciar.addEventListener('click', function (event) {
-  alert('Clique no iniciar')
-});
-
-pausar.addEventListener('click', function (event) {
-  alert('Clique no iniciar')
-});
-
-zerar.addEventListener('click', function (event) {
-  alert('Clique no iniciar')
-});
+relogio();
