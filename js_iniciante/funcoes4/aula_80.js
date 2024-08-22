a518a6c2dff3af043c3472c4f74b6e74c6898291
Julia Stella <julia.paula@liferay.com>
2024-08-22 18:41:08 -0300

    geradora parte 1

--- js_iniciante/funcoes4/aula_80.js
+++ js_iniciante/funcoes4/aula_80.js
@@ -1,8 +1,35 @@
 // função geradora
 
 function* geradora1() {
+    // codigo qualquer ...
     yield 'valor 1'
-
-    
+    // codigo qualquer ...
+    yield 'valor 2'
+    // codigo qualquer ...
+    yield 'valor 3'
 }
 
+// const g1 = geradora1();
+// console.log(g1.next().value);
+// console.log(g1.next().value);
+// console.log(g1.next());
+// console.log(g1.next());
+
+// exemplo 2
+function* gerador2() { // esse gerador é infinito, o que não é legal
+    let i = 0;
+
+    while(true){
+        yield i;
+        i++;
+    }
+}
+
+const g2 = gerador2();
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
