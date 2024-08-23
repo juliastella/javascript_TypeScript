c6aea9c4258749160a2ad0575a8d483ecf7287ee
Julia Stella <julia.paula@liferay.com>
2024-08-23 14:33:53 -0300

    Finalização do modulo de funções

--- js_iniciante/funcoes4/aula_80.js
+++ js_iniciante/funcoes4/aula_80.js
@@ -33,3 +33,47 @@ console.log(g2.next().value);
 console.log(g2.next().value);
 console.log(g2.next().value);
 console.log(g2.next().value);
+
+
+// exemplo 3
+
+function* geradora3() {
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function* geradora4() {
+    yield* geradora3(); // tenho que colocar o * para informa que estou chamando outra função geradora
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4();
+for(let valor of g4){
+    console.log(valor);
+}
+
+// exemplo 3
+function geradora5() {
+    yield function() {
+        console.log('teste 1');
+    };
+
+    return function() {
+      console.log('teste return'); // o return mata todos os proximos yield    
+    };
+
+    yield function() {
+        console.log('teste 2');  
+    };
+}
+
+const g5 = geradora5();
+const func1 = g5.next().value;
+const func2 = g5.next().value;
+
+func1();
+func2();
+func3();
