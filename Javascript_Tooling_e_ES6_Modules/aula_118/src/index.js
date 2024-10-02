e39e418780bdd9d7baec826737c15f9867d61ff4
Julia Stella <julia.paula@liferay.com>
2024-10-02 17:39:51 -0300

    exporte e importe

---
+++ Javascript_Tooling_e_ES6_Modules/aula_118/src/index.js
@@ -0,0 +1,8 @@
+import {nome as nome2,sobrenome, idade, soma, Pessoa} from './modulo1';
+
+// um exemplo para renome algo que é importado é colocar os "as" e logo depois o nome da nova variavel
+
+const p1 = new Pessoa('luiz', 'rw');
+
+console.log(p1);
+
