73e1b269bf7b79607437991c6688afb21dbcd9ff
Julia Stella <julia.paula@liferay.com>
2024-10-23 23:15:45 -0300

    finalização da estilização

--- react_basico/src/components/Main.js
+++ react_basico/src/components/Main.js
@@ -1,29 +1,60 @@
 import React, { Component } from "react";
 
+//icone
+import {FaPlus, FaWindowClose, FaEdit} from 'react-icons/fa';
+
+import './Main.css';
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      novaTarefa: '',
-    };
-    this.inputMudou = this.inputMudou.bind(this);
+
+  state = { // essa é a forma mai facil
+    novaTarefa: '',
+    tarefas: [
+
+    ],
+  };
+
+  handleChange = (e) => {
+    e.preventDefault();
+    console.log('oi');
+
   }
 
-  inputMudou(e) {
+  handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
     });
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
     return (
       <div className="main">
-        <h1>{novaTarefa}</h1>
-        <form action="#">
-          <input onChange={this.inputMudou} type="text" />
-          <button type="submit">Enviar</button>
+        <h1>Lista de tarefas</h1>
+
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+            />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+
+        <ul className="tarefas">
+            {tarefas.map(tarefa => (
+              <li key={ tarefa }>
+                {tarefa}
+
+                <span>
+                  <FaEdit className="edit"/>
+                  <FaWindowClose className="delete"/>
+                </span>
+              </li>
+            ))}
+        </ul>
       </div>
     );
   }
