9019f5bf0c4fd96b7ca39381e6d49aea1b65172d
Julia Stella <julia.paula@liferay.com>
2024-10-29 16:28:21 -0300

    Finalização da ligação

--- node_npm_espress/aula_140/src/controllers/homeController.js
+++ node_npm_espress/aula_140/src/controllers/homeController.js
@@ -1,8 +1,17 @@
+const HomeModel = require("../models/HomeModel");
+
+HomeModel.create({
+  titulo: "um titulo de testes",
+  descricao: "Uma descrição de teste",
+})
+  .then((dados) => console.log(dados))
+  .catch((e) => console.log(e));
+
 exports.paginaInicial = (req, res) => {
-    res.render('index');
+  res.render("index");
 };
 
 exports.trataPost = (req, res) => {
-    res.send(req.body);
-    return;
+  res.send(req.body);
+  return;
 };
