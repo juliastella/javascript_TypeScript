55200d81bcfec932a56e6b9d0d7b4361b2fade0a
Julia Stella <julia.paula@liferay.com>
2024-12-19 16:32:18 -0300

    melhorias no CRUD user

--- api_rest/src/controllers/UserController.js
+++ api_rest/src/controllers/UserController.js
@@ -1,11 +1,11 @@
-import { UUID } from 'sequelize';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      return res.json(novoUser);
+      const { id, nome, email } = novoUser;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -13,29 +13,68 @@ class UserController {
     }
   }
 
-  // index
-  async  index(req, res) {
-    try{
-      const users = await User.findAll();
-      return res.json(users)
-    }
-    catch (e){
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
+      return res.json(users);
+    } catch (e) {
       return res.json(null);
     }
   }
 
-  //show
+  // Show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
 
-  async  show(req, res) {
-    try{
-      const user =  await User.findByPk(req.params.id);
-      return res.json(user)
-    }
-    catch (e){
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
+    } catch (e) {
       return res.json(null);
     }
   }
 
+  // Update
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      const { id, nome, email } = novosDados;
+      return res.json({ id, nome, email });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(null);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new UserController();
--- api_rest/src/routes/userRoutes.js
+++ api_rest/src/routes/userRoutes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
-router.post('/', userController.store);
-router.get('/', userController.index);
-router.get('/', userController.show);
+// Não deveria existir
+// router.get('/', userController.index); // Lista usuários
+// router.get('/:id', userController.show); // Lista usuário
 
+router.post('/', userController.store);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired, userController.delete);
 
 export default router;
 
